51bfe1a92806feee4b53f94a895dd2327242049d
Shivam Gupta <shivamaggarwal767@gmail.com>
2023-05-21 00:01:32 +0530

    removed constructor

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,8 +2,7 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args){
-    RWOnly myName = new RWOnly("Shivam");
-    System.out.println(myName.getName());
+    RWOnly myName = new RWOnly();
     myName.setName("Ayush");
     System.out.println(myName.getName());
   }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,9 +2,6 @@ package com.driver;
 
 public class RWOnly {
     private String name;
-    public RWOnly(String name){
-        this.name = name;
-    }
     public String getName(){
         return this.name;
     }
