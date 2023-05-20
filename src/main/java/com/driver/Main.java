02acfd7dac0baefccee4a799fc7c343659cfbd89
Shivam Gupta <shivamaggarwal767@gmail.com>
2023-05-20 23:46:22 +0530

    initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,8 @@
 package com.driver;
 
 public class Main {
-  
+  RWOnly myName = new RWOnly("Shivam");
+  System.out.println(myName.getName());
+  myName.setName("Ayush");
+  System.out.println(myName.getName());
 }
