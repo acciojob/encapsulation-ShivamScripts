01a39ffc287e31c4a696b60e6b664a7499f06bc8
Shivam Gupta <shivamaggarwal767@gmail.com>
2023-06-08 17:29:24 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
   public static void main(String[] args){
     RWOnly myName = new RWOnly();
-    myName.setName("Ayush");
+    myName.setName("Shivam");
     System.out.println(myName.getName());
   }
 }
